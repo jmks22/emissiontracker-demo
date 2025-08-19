5f6eb54cc07fb4dc9cfe915fb20fb02882357331
jmks22 <jmeeks_22@outlook.com>
2025-08-19 12:08:03 +0500

    Force update docs folder for GitHub Pages

--- docs/assets/md.transition-legacy-w_tEcDoY.js
+++ docs/assets/md.transition-legacy-w_tEcDoY.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-bfKr20EQ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BJBdFSO_.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
