682fb694771a3c2ef3da51efbd19e9e739706370
jmks22 <jmeeks_22@outlook.com>
2025-08-19 10:38:06 +0500

    Prepare for GitHub Pages deploy

--- emission_tracker/vite.config.ts
+++ emission_tracker/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: process.env.NODE_ENV === 'production' ? '/emission_tracker/' : '/',
   plugins: [
     react(),
     legacy()
